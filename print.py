564de46bdb46ecb4974b9042cb445ea021ae1abe
sunuwars77 <sunuwars762@gmail.com>
2022-11-07 21:47:00 +0545

    edit comment

--- print.py
+++ print.py
@@ -1,5 +1,5 @@
-syntax of print function 
-print(value,....,sep='',end='\n') (by default)
+#syntax of print function 
+#print(value,....,sep='',end='\n') (by default)
 
 print (5,6,7, sep='-',end='$')
 print("testing")
