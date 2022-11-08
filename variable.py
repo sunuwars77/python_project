f6b2bfbf85a45603a26b641b1a040a283bc7d65a
sunuwars77 <sunuwars762@gmail.com>
2022-11-08 21:42:39 +0545

    variables

---
+++ variable.py
@@ -0,0 +1,25 @@
+#variables containers for storing data values(eg bag)
+# declaration is notrequired (int a=5 not require , we can write a=5)
+# dynamic type programming language
+
+
+#eg 1
+a=12
+b=2
+c=a+b
+print (c)
+
+#eg 2
+x=2
+print(x)
+
+
+#assign value to multiple varibles
+
+#eg 1
+a,b,c=45,54,34
+print (a,b,c)
+
+#single value to multiple variables
+a=b=c=12
+print (a,b,c)
