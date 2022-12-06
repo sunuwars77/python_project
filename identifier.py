8b2921ebb469e101c5c47c940c86e5752faa47a7
sunuwars77 <sunuwars762@gmail.com>
2022-12-06 08:59:48 +0545

    variables

---
+++ identifier.py
@@ -0,0 +1,13 @@
+#identifiers
+#(it is a name given to entities like class,functions,variables eg x=10 ,here identifier identify that x isa variable)
+#useIDLE Python
+
+# to check invalid identifier
+"12number".isidentifier()
+false
+
+
+#to check valid identifier
+"name" .isidentifier()
+true
+
